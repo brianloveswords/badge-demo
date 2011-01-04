ef3b67f20372c26db069b61b6e4c63b4a323f5d1
Brian Brennan <brianloveswords@gmail.com>
2011-01-04 15:45:33 -0500

    Issuer!

---
+++ issuer/badges.js
@@ -0,0 +1,40 @@
+(function(global){
+  global.badges = {
+    audio: {
+      title: "Audio Editor",
+      issuer: "https://www.nwp.org/",
+      issuerName: "National Writing Project",
+      imageURL:"http://localhost/issuer/images/nwp_audio_editor_badge.png",
+      badgeURL:"https://www.nwp.org/audioeditor"
+    },
+    
+    science: {
+      title: "Science Journalist",
+      issuer: "https://www.nwp.org/",
+      issuerName: "National Writing Project",
+      imageURL:"http://localhost/issuer/images/nwp_science_journalist_badge.png",
+      badgeURL:"https://www.nwp.org/sciencejournalist"
+    },
+    video: {
+      title: "Video Editor",
+      issuer: "https://www.nwp.org/",
+      issuerName: "National Writing Project",
+      imageURL:"http://localhost/issuer/images/nwp_video_editor_badge.png",
+      badgeURL:"https://www.nwp.org/videoeditor"
+    },
+    accessibility: {
+      title: "Accessibility Expert",
+      issuer: "https://www.p2pu.org/",
+      issuerName: "Peer 2 Peer University",
+      imageURL:"http://localhost/issuer/images/p2pu_accessibility_expert_badge.png",
+      badgeURL:"https://www.p2pu.org/accessibility"
+    },
+    javascript: {
+      title: "Javascript Expert",
+      issuer: "https://www.p2pu.org/",
+      issuerName: "Peer 2 Peer University",
+      imageURL:"http://localhost/issuer/images/p2pu_javascript_expert_badge.png",
+      badgeURL:"https://www.javascript.org/javascript"
+    }
+  };
+})(this);
